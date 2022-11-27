261b8dc5be1829ec391904e9d2129b3ac628a496
Jesus_hna <thehacha167@gmail.com>
2022-11-28 00:38:51 +0100

    agregar archivos

---
+++ t5/ejercicio1.c
@@ -0,0 +1,9 @@
+//Implementa un programa que lea un número positivo por teclado e informe de si el número es par o impar.
+
+#include <stdio.h>
+
+int main (int argc,char **argv)
+{
+   printf("Hola mundo\n");
+   return 0;
+}
