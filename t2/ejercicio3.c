54c31b5ae7b64ed8f66e322ec803f46cd4a8b1cc
Jesus_hna <thehacha167@gmail.com>
2022-11-28 12:58:20 +0100

    error

--- t2/ejercicio3.c
+++
@@ -1,9 +0,0 @@
-//Implementa un programa que lea la edad de una persona menor a 50 años e informe de si es un niño (0-12 años), un adolescente (12-17), un joven (18-29) o un adulto.
-
-#include <stdio.h>
-
-int main (int argc,char **argv)
-{
-   printf("Hola mundo\n");
-   return 0;
-}
