da3d7d561b13bd23220a282a7f20abd54eb34f64
Jesus_hna <thehacha167@gmail.com>
2022-11-28 14:38:54 +0100

    t2 8,9

--- t2/ejercicio8.c
+++ t2/ejercicio8.c
@@ -1,9 +1,25 @@
-//Implementa un programa que muestre por pantalla todos los números comprendidos entre 1 y 100 que son múltiplos de 7 o de 13.
+// Implementa un programa que muestre por pantalla todos los números comprendidos entre 1 y 100 que son múltiplos de 7 o de 13.
 
 #include <stdio.h>
 
-int main (int argc,char **argv)
+int main()
 {
-   printf("Hola mundo\n");
+   for (size_t i = 0; i < 100; i++)
+   {
+
+      if (i % 7 == 0)
+      {
+         printf("Multiplo de 7     %i\n", i);
+      }
+      else
+      {
+
+         if (i % 13 == 0)
+         {
+            printf("Multiplo de 13    %i\n", i);
+         }
+      }
+   }
+
    return 0;
 }
--- t2/ejercicio9.c
+++ t2/ejercicio9.c
@@ -1,9 +1,31 @@
-//Implementa un programa que averigüe si un número N es primo.
-
+// Implementa un programa que averigüe si un número N es primo.
 #include <stdio.h>
-
-int main (int argc,char **argv)
+#include <stdbool.h>
+bool primo = true;
+int n1 = 0;
+int main()
 {
-   printf("Hola mundo\n");
+
+   printf("Introduce el numero a comprobar \n");
+   scanf("%i", &n1);
+   if (n1 == 0 || n1 == 1 || n1 == 4)
+   {
+      return 0;
+   }
+   for (int x = 2; x < n1 / 2; ++x)
+   {
+      if (n1 % x == 0)
+      {
+         primo = false;
+      }
+   }
+   if (primo == 1)
+   {
+      printf("Es primo");
+   }
+   else
+   {
+      printf("No es primo");
+   }
    return 0;
 }
