71e6db134b2e5625bee459fe27e901d7cbf274df
Jesus_hna <thehacha167@gmail.com>
2022-11-28 13:38:32 +0100

    t2 4,5

--- t2/ejercicio4.c
+++ t2/ejercicio4.c
@@ -1,9 +1,19 @@
-//Implementa un programa que lea dos números enteros cualesquiera y muestre los números comprendidos entre ambos, sin incluirlos a ellos mismos.
+// Implementa un programa que lea dos números enteros cualesquiera y muestre los números comprendidos entre ambos, sin incluirlos a ellos mismos.
 
 #include <stdio.h>
+int n1 = 0, n2 = 0;
 
-int main (int argc,char **argv)
+int main()
 {
-   printf("Hola mundo\n");
+   printf("Da el primer numero\n");
+   scanf("%i", &n1);
+   printf("Da el segundo numero\n");
+   scanf("%i", &n2);
+
+   for (int i = 1; i <= (n2 - 1); i++)
+   {
+      printf("%i\n", n1 + i);
+   }
+
    return 0;
 }
--- t2/ejercicio5.c
+++ t2/ejercicio5.c
@@ -1,9 +1,17 @@
-//Implementa un programa que muestre la tabla de multiplicar de un número entero.
+// Implementa un programa que muestre la tabla de multiplicar de un número entero.
 
 #include <stdio.h>
+int n1 = 0;
 
-int main (int argc,char **argv)
+int main()
 {
-   printf("Hola mundo\n");
+   printf("Da  primer numero a obtener la tabla\n");
+   scanf("%i", &n1);
+
+   for (int i = 0; i < 11; i++)
+   {
+      printf("%i\n", n1 * i);
+   }
+
    return 0;
 }
