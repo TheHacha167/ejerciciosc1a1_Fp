7828e09be0f9675be6bec413672f2859e860526c
Jesus_hna <thehacha167@gmail.com>
2022-11-28 13:53:14 +0100

    t2 6,7

--- t2/ejercicio6.c
+++ t2/ejercicio6.c
@@ -1,9 +1,15 @@
-//Implementa un programa que genere números enteros de 3 en 3 comenzando por el 2 hasta el valor máximo menor que 30.
+// Implementa un programa que genere números enteros de 3 en 3 comenzando por el 2 hasta el valor máximo menor que 30.
 
 #include <stdio.h>
+int n1 = 2;
 
-int main (int argc,char **argv)
+int main()
 {
-   printf("Hola mundo\n");
+   for (int i = 0; i < 9; i++)
+   {
+      n1 = n1 + 3;
+      printf("%i\n", n1);
+   }
+
    return 0;
 }
--- t2/ejercicio7.c
+++ t2/ejercicio7.c
@@ -1,9 +1,17 @@
-//Implementa un programa que lea un número N y muestre la suma de los N primeros números.
+// Implementa un programa que lea un número N y muestre la suma de los N primeros números.
 
 #include <stdio.h>
+int n1 = 0, n2 = 0;
 
-int main (int argc,char **argv)
+int main()
 {
-   printf("Hola mundo\n");
+
+   printf("Introduce el numero\n");
+   scanf("%i", &n1);
+   for (int i = 0; i < (n1 + 1); i++)
+   {
+      printf("%i\n", n2);
+      n2 = n2 + 1;
+   }
    return 0;
 }
