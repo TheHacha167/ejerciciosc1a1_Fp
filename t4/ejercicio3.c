4f8fe0c56ff6801ac4206f789f2dc39ae2d2fa1a
Jesus_hna <thehacha167@gmail.com>
2022-11-30 10:37:39 +0100

    tema 4 acabado

--- t4/ejercicio3.c
+++ t4/ejercicio3.c
@@ -9,4 +9,22 @@ int main()
 {
     printf("Da  primer numero a comparar\n");
     scanf("%i", &n1);
+
+    printf("Da  primer numero a comparar\n");
+    scanf("%i", &n2);
+    if (n1 > n2)
+    {
+        printf("El primer numero es mayor %i\n", n1);
+    }
+    else
+    {
+        if (n2 > n1)
+        {
+            printf("El segundo numero es mayor %i\n", n2);
+        }
+        else
+        {
+            printf("Son iguales los numeros \n");
+        }
+    }
 }
--- t4/ejercicio6.c
+++ t4/ejercicio6.c
@@ -1,11 +1,23 @@
 /*
-    Implementa un programa que genere números enteros de 2 en 2 comenzando por el 2 hasta el valor máximo menor que 30 y sume los divisibles por 2.
+    Implementa un programa que genere números enteros de 2 en 2 comenzando
+    por el 2 hasta el valor máximo menor que 30 y sume los divisibles por 2.
 
 */
 #include <stdio.h>
+int n1 = 2, n2 = 0;
 
-int main (int argc,char **argv)
+int main()
 {
-   printf("Hola mundo\n");
-   return 0;
+    for (int i = 0; i < 14; i++)
+    {
+        n1 = n1 + 2;
+        printf("%i\n", n1);
+        if (n1 % 2 == 0)
+        {
+            n2 = n2 + n1;
+        }
+    }
+    printf("......................\n");
+    printf("%i La suma de todos los numeros anteriores divisibles por dos es : \n", n2);
+    return 0;
 }
