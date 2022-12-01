b313df40daee2fb301f5599cfd9e421505563e52
Jesus_hna <thehacha167@gmail.com>
2022-12-01 23:41:52 +0100

    t6 ej 1

--- t6/ejercicio1.c
+++ t6/ejercicio1.c
@@ -3,9 +3,21 @@ Implementa un programa que introduzca los valores de un array mediante un bucle 
 
 */
 #include <stdio.h>
-
-int main (int argc,char **argv)
+int n1 = 0;
+int valores[];
+int main()
 {
-   printf("Hola mundo\n");
+   printf("introduzca el numero de datos que desea introducir\n");
+   scanf("%i", &n1);
+   for (size_t i = 0; i < n1; ++i)
+   {
+      printf("introduzca el %i valor a almacenar :\n", i);
+      scanf("%i", &valores[i]);
+   }
+   printf("los valores introducidos corresponden a :\n");
+   for (size_t i = 0; i < n1; ++i)
+   {
+      printf("%i\n", valores[i]);
+   }
    return 0;
 }
