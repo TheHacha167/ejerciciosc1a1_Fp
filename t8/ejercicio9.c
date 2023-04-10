8d940eea41d6a4e5bde6f831baa3ca2ebf086bc3
Jesus_hna <thehacha167@gmail.com>
2023-04-10 16:01:28 +0200

    Exención de responsabilidad:
    Exención de responsabilidad:

--- t8/ejercicio9.c
+++ t8/ejercicio9.c
@@ -1,10 +1,34 @@
 /*
+  Implementa un programa en C que muestre el uso de la función ferror() en ficheros.
 
 */
 #include <stdio.h>
 
-int main (int argc,char **argv)
-{
-   printf("Hola mundo\n");
-   return 0;
+int main() {
+    FILE *fichero;
+    char caracter;
+
+    // Se abre el fichero en modo lectura
+    fichero = fopen("ejemplo.txt", "r");
+
+    if(fichero == NULL) {
+        printf("Error al abrir el fichero\n");
+        return 1;
+    }
+
+    // Se intenta leer un carácter del fichero
+    caracter = fgetc(fichero);
+
+    // Se comprueba si se ha producido un error durante la lectura del carácter
+    if(ferror(fichero)) {
+        printf("Se ha producido un error durante la lectura del fichero\n");
+        return 1;
+    }
+
+    // Se cierra el fichero
+    fclose(fichero);
+
+    printf("El primer carácter del fichero es '%c'\n", caracter);
+
+    return 0;
 }
